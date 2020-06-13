64a45603da1ba33816409ed9fb63bda8638e9c13
Donghyun <markk628@gmail.com>
2020-06-12 17:37:36 -0700

    v1.0 complete

--- makesite.go
+++ makesite.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"html/template"
+	"flag"
 	"os"
+	"html/template"
+	"io/ioutil"
+	"strings"
 )
 
 type post struct {
-	User string
 	Content string
 }
 
@@ -23,23 +23,46 @@ func readFile(templateName string) string {
 	return string(fileContents)
 }
 
-func renderTemplate(content string) *template.Template {
-	path:= []string {
-		"template.tmpl",
-	}
+func renderTemplate(filename string, data string) {
+	c := post{Content: data}
+	t := template.Must(template.New("template.tmpl").ParseFiles(filename))
 
-	templateFile := template.Must(template.New("template.tmpl").ParseFiles(path...))
-	err := templateFile.Execute(os.Stdout, post{User: "John Doe", Content: content})
+	var err error
+	err = t.Execute(os.Stdout , c)
 	if err != nil {
 		panic(err)
 	}
-	return templateFile
+}
+
+func addExtHTML(filename string) string {
+	ext := ".html"
+	withExt := strings.Split(filename, ".")[0] + ext
+	return withExt
+}
+
+func writeTemplateToFile(tmplName string, data string) {
+	p := post{Content: readFile(data)}
+	t := template.Must(template.New("template.tmpl").ParseFiles(tmplName))
+
+	file := addExtHTML(data)
+	f, err := os.Create(file)
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.Execute(f, p)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	fmt.Println("Evenin lads")
-
-	content := readFile("first-post.txt")
-	t := renderTemplate(content)
-	print(t)
+	filePtr := flag.String("file", "", "txt file to be converted to html file")
+	flag.Parse()
+	if *filePtr != "" {
+		renderTemplate("template.tmpl", readFile(*filePtr))
+		writeTemplateToFile("template.tmpl", *filePtr)
+	} else {
+		print("run what?")
+	}
 }
